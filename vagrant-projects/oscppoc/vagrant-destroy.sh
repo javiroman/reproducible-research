a7e05121f5724839dab8af9613fd2d9a9153f137
Javi Roman <jroman.espinar@gmail.com>
2016-09-16 14:37:01 +0200

    new role mn

--- vagrant-projects/oscppoc/vagrant-destroy.sh
+++
@@ -1,5 +0,0 @@
-vagrant destroy oscppoc-mn
-vagrant destroy oscppoc-master
-vagrant destroy oscppoc-node1
-vagrant destroy oscppoc-node2
-
--- vagrant-projects/oscppoc/vagrant-up.sh
+++
@@ -1,7 +0,0 @@
-vagrant up oscppoc-mn --no-provision
-vagrant up oscppoc-master --no-provision
-vagrant up oscppoc-node1 --no-provision
-vagrant up oscppoc-node2 --no-provision
-
-echo "now manually register the systems ..."
-echo "... and vagrant provision finally"
