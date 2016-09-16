78e03eac565925d8733b2aac6b3e3e770eaa57a1
Javi Roman <jroman.espinar@gmail.com>
2016-09-16 12:07:10 +0200

    fixed #issue 651 vagrant-libvirt

--- vagrant-projects/oscppoc/vagrant-destroy.sh
+++ vagrant-projects/oscppoc/vagrant-destroy.sh
@@ -1,3 +1,4 @@
+vagrant destroy oscppoc-mn
 vagrant destroy oscppoc-master
 vagrant destroy oscppoc-node1
 vagrant destroy oscppoc-node2
--- vagrant-projects/oscppoc/vagrant-up.sh
+++ vagrant-projects/oscppoc/vagrant-up.sh
@@ -1,3 +1,4 @@
+vagrant up oscppoc-mn --no-provision
 vagrant up oscppoc-master --no-provision
 vagrant up oscppoc-node1 --no-provision
 vagrant up oscppoc-node2 --no-provision
