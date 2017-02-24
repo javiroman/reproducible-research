e8543dc52c8eb280e34d269acc5e496dfbec60f1
Javi Roman <jroman.espinar@gmail.com>
2017-02-24 20:03:52 +0100

    dynamic external inventory for vagrant

--- vagrant-projects/ocpha/create-inventory.sh
+++ vagrant-projects/ocpha/create-inventory.sh
@@ -1,11 +1,16 @@
-vagrant ssh-config > /tmp/a.dat
+vagrant ssh-config > /tmp/ssh-config.dat
 
-host_list=$(cat /tmp/a.dat | grep ^Host | cut -d' ' -f2)
+host_list=$(cat /tmp/ssh-config.dat | grep ^Host | cut -d' ' -f2)
 
-echo "[all:vars]" > file
+echo "[all:vars]" > inventory
+echo "ansible_ssh_user=vagrant" >> inventory
+echo "ansible_become=yes" >> inventory
+echo "[hosts]" >> inventory
 for i in $host_list; do
-    echo -ne "$i ansible_host="
-    cat /tmp/a.dat | grep -A 1 "^Host ${i}$" | grep HostName | cut -d' ' -f4
-done >> file
+    echo -ne "$i ansible_ssh_private_key_file=.vagrant/machines/$i/libvirt/private_key ansible_host="
+    cat /tmp/ssh-config.dat | grep -A 1 "^Host ${i}$" | grep HostName | cut -d' ' -f4
+done >> inventory
 
-rm /tmp/a.dat
+rm /tmp/ssh-config.dat
+
+# http://stackoverflow.com/questions/32297456/how-to-ignore-ansible-ssh-authenticity-checking
